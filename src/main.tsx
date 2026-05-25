345c3a4f0c8fc67e490b066d57d8df5740f9de60
DKlarations <dklarations@gmail.com>
2026-05-25 01:37:39 -0500

    Update displayed app version

--- src/main.tsx
+++ src/main.tsx
@@ -239,7 +239,7 @@ function App() {
           <div>
             <div className="title-row">
               <h1>RRG Pull List Formatter</h1>
-              <span>v0.2.4</span>
+              <span>v0.2.5</span>
             </div>
           </div>
           <div className="logo-slot logo-slot-right" aria-hidden="true">
